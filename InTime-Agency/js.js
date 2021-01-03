394f59c03ca03e7ef08389f32a42b9e4bcd9224d
Alex Nazaretyan <alex.nazaretyan@abv.bg>
2021-01-03 10:13:17 +0200

    deleting files

--- InTime-Agency/js.js
+++
@@ -1,6 +0,0 @@
-let solve = lastDigit =>{
-    let toString = lastDigit.toString();
-    let theLastDigit = Number(toString[toString.length-1]); 
-    return theLastDigit
-}
-console.log(solve(20443023));
